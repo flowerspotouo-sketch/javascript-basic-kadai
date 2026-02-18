f09e87ea8a7b8be2d7a6d1c9b0e7bed8c963e50c
flowerspotouo-sketch <flowerspotouo@gmail.com>
2026-02-18 21:52:59 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,8 @@
+console.log("これは正しい bmi.js です");
+
+const weight = 68; // 体重(kg)
+const height = 1.7; // 身長(m)
+
+const bmi = weight / (height * height);  // BMIを計算して変数に格納
+
+console.log(bmi); // 結果をコンソールに出力
